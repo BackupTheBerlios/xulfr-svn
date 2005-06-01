fc07111fe68bd44ceb4d0cc8520faa65a8d87497
laurentj <laurentj@88952adc-64f8-0310-be1c-f3ed2c412a41>
2005-06-01 09:58:22 +0000

    nomsg

--- trunk/statiques/exemples/findfile/findfile-dom.js
+++
@@ -1,8 +0,0 @@
-function doFind()
-{
-  var meter=document.getElementById('progmeter');
-  meter.hidden = false;
-  var searchtext=document.getElementById('find-text').value;
-  alert("Searching for \""+searchtext+"\"");
-}
-
