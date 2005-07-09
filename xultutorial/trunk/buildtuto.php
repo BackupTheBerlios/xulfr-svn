74d21541324ce54d0f4df28242e95dc8df46afdd
chbok <chbok@88952adc-64f8-0310-be1c-f3ed2c412a41>
2005-07-09 13:26:17 +0000

    Traduction et relecture du tuto 8.1

--- xultutorial/trunk/buildtuto.php
+++ xultutorial/trunk/buildtuto.php
@@ -73,10 +73,10 @@ $article_list=array(
     'dragex'=>array('7.10 Exemple Drag and Drop',                           array('Laurent Jouanneau'=>'11/11/2004'),'dragwrap','trees'),
 
 
- 'trees' =>array('8.1 Arbres',                                          array('Damien Hardy'=> '10/04/2004'),'xpcomex','advtrees'),
+ 'trees' =>array('8.1 Arbres',                                          array('Damien Hardy'=> '10/04/2004', 'Alain B.'=>'08/07/2005'),'xpcomex','advtrees'),
  'advtrees' =>array('8.2 Autres caractéristiques des arbres',           array('Laurent Jouanneau'=> '24/06/2004', 'Gerard L.'=>'25/03/2005'),'trees','seltree'),
  'seltree'=>array('8.3 Selection dans les arbres',                      array('Medspx'=>'17/07/2004', 'Alain B.'=>'03/07/2005'),'advtrees','treeview'),
- 'treeview'=>array('8.4 Vues d\'arbre personnalisées',                  array('Chaddaï Fouché'=>'20/07/2004'),'seltree','treeviewdet'),
+ 'treeview'=>array('8.4 Vues d\'arbre personnalisées',                  array('Chaddaï Fouché'=>'20/07/2004', 'Alain B.'=>'08/07/2005'),'seltree','treeviewdet'),
         'treeviewdet'=>array('8.5 Détails sur les vues d\'arbres',     array('Romain D.'=>'03/05/2005'),'treeview','treeboxobject'),
         'treeboxobject'=>array('8.6 Les objets boîtes des arbres',     array('Alain B.'=>'15/06/2005'),'treeviewdet','intrordf'),
 
