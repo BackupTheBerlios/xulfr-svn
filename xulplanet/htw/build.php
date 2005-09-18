7487529da61446fc7fa0c57ef7f2dec92dffad4e
chbok <chbok@88952adc-64f8-0310-be1c-f3ed2c412a41>
2005-09-18 13:22:42 +0000

    Traduction de HTW XVI

--- xulplanet/htw/build.php
+++ xulplanet/htw/build.php
@@ -33,7 +33,7 @@ $article_list=array(
  'HTW XIII'   =>array(312, 'Autre syntaxe d\'Action',     array('Alain B.'=>'17/09/2005') ),
  'HTW XIV'    =>array(313, 'Littéraux dans les triplets',      array('Alain B.'=>'17/09/2005') ),
  'HTW XV'     =>array(314, 'Navigation dans la source',       array('Alain B.'=>'17/09/2005') ),
- 'HTW XVI'    =>array(315, 'Navigation supplémentaire et filtrage',      array(''=>'') ),
+ 'HTW XVI'    =>array(315, 'Navigation supplémentaire et filtrage',      array('Alain B.'=>'18/09/2005') ),
  'HTW XVII'   =>array(316, 'Générer un menu avec des filtres',     array(''=>'') ),
  'HTW XVIII'  =>array(317, 'Contenu statique',    array(''=>'') ),
  'HTW XIX'    =>array(318, 'Syntaxe d\'une règle simple',      array(''=>'') ),
