e19487aa013e0e087fb681f19d41989d08365122
chbok <chbok@88952adc-64f8-0310-be1c-f3ed2c412a41>
2005-10-04 09:51:13 +0000

    Traduction de HTW XXXII

--- xulplanet/htw/build.php
+++ xulplanet/htw/build.php
@@ -49,7 +49,7 @@ $article_list=array(
  'HTW XXIX'   =>array(330, 'Utilisation de règles multiples pour plus de résultats',     array('Alain B.'=>'03/10/2005') ),
  'HTW XXX'    =>array(331, 'Construction d\'arbres',      array('Alain B.'=>'03/10/2005') ),
  'HTW XXXI'   =>array(332, 'Fonctionnalités du constructeur d\'arbres',     array('Alain B.'=>'03/10/2005') ),
- 'HTW XXXII'  =>array(333, 'Arbres hiérarchiques',    array(''=>'') ),
+ 'HTW XXXII'  =>array(333, 'Arbres hiérarchiques',    array('Alain B.'=>'04/10/2005') ),
  'HTW XXXIII' =>array(334, 'Attachement d\'un constructeur de gabarits',   array(''=>'') ),
  'HTW XXXIV'  =>array(335, 'La reconstruction de gabarits',    array(''=>'') ),
  'HTW XXXV'   =>array(336, 'Les observateurs de gabarits',     array(''=>'') ),
